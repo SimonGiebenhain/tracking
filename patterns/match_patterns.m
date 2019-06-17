839d70fff1a8ed42d9b4a27a5111695691cf7254
SimonGiebenhain <simon.giebenhain@uni-konstanz.de>
2019-06-17 14:08:55 +0200

    bug fixes in match-patterns

--- patterns/match_patterns.m
+++ patterns/match_patterns.m
@@ -1,25 +1,39 @@
-function assignment = match_patterns(marker1, marker2)
+function assignment = match_patterns(whole_pattern, detected_pattern)
 %MATCH_PATTERNS Match points within two sets of points
 %   So far this method relies on a constant/known orientation of the point
 %   pattern.
+%   assignment(i) contains index of marker in whole_pattern assigned to the
+%   ith detected marker.
+%
 %   The algorithm creates edges representing the offset from one point to
 %   another. Then it compares these edges within the two sets of points and
 %   finds correspondances where the outgoing edges of a point are similar
 %   in both point sets.
 %   The algorithm is very simple and only a first draft.
 
-    edges1 = get_edges(marker1);
-    edges2 = get_edges(marker2);
+    edges1 = get_edges(whole_pattern);
+    self_edges_idx = repmat(sum(abs(edges1),3) == 0,1,1,2);
+    edges1(self_edges_idx) = Inf;
+    edges2 = get_edges(detected_pattern);
 
     num_nodes_1 = size(edges1,1);
     num_nodes_2 = size(edges2,1);
     cost_matrix = zeros(num_nodes_2, num_nodes_1);
+    
     for i = 1:num_nodes_2
         for j = 1:num_nodes_1
-            e2 = squeeze(edges2(i,:,:));
-            e1 = squeeze(edges1(j,:,:));
+            if size(edges1,3) == 1
+                e2 = edges2(i,:)';
+                e1 = edges1(j,:)';
+            else
+                e2 = squeeze(edges2(i,:,:));
+                e1 = squeeze(edges1(j,:,:));
+            end
             c = 0;
             for k = 1:size(e2,1)
+                if sum(e2(k,:)) == 0
+                    continue;
+                end
                 diff = sqrt(sum((e1-e2(k,:)).^2,2));
                 [min_val, min_idx] = min(diff);
                 c = c + min_val;
--- patterns/test_pattern_matching.m
+++ patterns/test_pattern_matching.m
@@ -1,5 +1,11 @@
 marker1 = [1 1; 0 0; 0 -2; -2 0];
+perm = randperm(size(marker1,1));
+perm = perm(1:3)
+marker2 = marker1(perm,:);
+marker2 = marker2 + 0.2*randn(size(marker2));
 
-marker2 = [1 1; 0 0; 0 -2; -2 0] + 0.5*randn(4,2);
+% m1 = [1; 0; -2; -4];
+% perm = randperm(size(m1,1))
+% m2 = m1(perm,:) + 0.5*randn(size(m1));
 
 assignemnt = match_patterns(marker1, marker2);
