524ff067dff9ec90c7a2f8b2b44f9a3788206b04
SimonGiebenhain <simon.giebenhain@uni-konstanz.de>
2019-09-24 22:07:54 +0200

    added rotation

--- modernMethods/markerToPositionPrediction.py
+++ modernMethods/markerToPositionPrediction.py
@@ -2,18 +2,29 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+
+from pyquaternion import Quaternion as Quaternion
+
+#from torchsummary import summary
 import numpy as np
 from viz import visualize
 
 
-N = 1000
+
+train_size = 5000
+test_size = 1000
 T = 200
-hidden_dim = 20
-embedding_dim = 15
-input_dim = 3
+
+dim = 3
+input_dim = 12
+fc1_dim = 20
+embedding_dim = 25
+hidden_dim = 30
+fc2_dim = 20
+output_dim = 3
 
 NUM_EPOCHS = 20
-BATCH_SIZE = 32
+BATCH_SIZE = 64
 
 
 #TODO 1  : generate data which is similar to birds trajectories?
@@ -40,16 +51,17 @@ def gen_training_data():
     #slopes = range(N)
     #xaxis = np.arange(T)
 
-    X_train = np.zeros([T, N, 3], dtype=np.float32)
-    X_test = np.zeros([T, N, 3], dtype=np.float32)
+    X_train = np.zeros([T, train_size, 3], dtype=np.float32)
+    X_test = np.zeros([T, test_size, 3], dtype=np.float32)
     #for n,a in enumerate(slopes):
     #    X[:, n ,0] = xaxis
     #    X[:, n ,1] = a/10 * xaxis
     #    X[:, n, 2] = np.sin(a/10 * xaxis/10)
 
-    for n in range(N):
-        X_train[:, n, :] = Gen_Spirals(T, input_dim)
-        X_test[:, n, :] = Gen_Spirals(T, input_dim)
+    for n in range(train_size):
+        X_train[:, n, :] = Gen_Spirals(T, dim)
+    for n in range(test_size):
+        X_test[:, n, :] = Gen_Spirals(T, dim)
 
     maxi1 = max(np.max(X_train[:,:,0]), np.max(X_test[:,:,0]))/5
     maxi2 = max(np.max(X_train[:,:,1]), np.max(X_test[:,:,1]))/5
@@ -63,26 +75,60 @@ def gen_training_data():
     return X_train, X_test, maxi1, maxi2, maxi3
 
 
-def add_markers_to_trajectories(trajectory, pattern):
+def gen_quats():
+    quats_train = np.zeros([T, train_size, 4])
+    quats_test = np.zeros([T, test_size, 4])
+
+
+    def gen_quat():
+        quat = np.zeros([T,4])
+        xaxis = np.linspace(0, np.pi, T)
+        quat[:, 0] = np.sin(xaxis/np.random.uniform(1,3))*np.random.uniform(1,10)
+        quat[:, 1] = xaxis / T * np.random.uniform(-3,3)
+        quat[:, 2] = np.sin(xaxis/np.random.uniform(1,3))**(np.random.uniform(0.1,3))
+        quat[:, 3] = np.arctan(xaxis/np.random.uniform(1,3))*np.random.uniform(0,5)
+
+        return quat / np.sqrt(np.sum(np.square(quat), axis=0))
+
+
+    for n in range(train_size):
+        quats_train[:, n, :] = gen_quat()
+    for n in range(test_size):
+        quats_test[:, n, :] = gen_quat()
+
+    return quats_train, quats_test
+
+
+def add_markers_to_trajectories(trajectory, quats, pattern):
     T = len(trajectory)
     N = np.shape(trajectory)[1]
 
-    marker0 = trajectory + pattern[0, :]
-    marker1 = trajectory + pattern[1, :]
-    marker2 = trajectory + pattern[2, :]
-    marker3 = trajectory + pattern[3, :]
+    detections = np.zeros([T, N, 12])
+    for t in range(T):
+        for n in range(N):
+            quat = Quaternion(quats[t,n,:])
+            rot_mat = quat.rotation_matrix
+            rotated_pattern = np.dot(rot_mat, pattern.T).T
+            detections[t, n, :] = np.reshape(rotated_pattern + trajectory[t, n], -1)
 
-    detections = np.concatenate([marker0, marker1, marker2, marker3], axis=2)
-    print(np.shape(detections))
+    #marker0 = trajectory + pattern[0, :] + np.random.normal(0, 0.02, np.shape(trajectory))
+    #marker1 = trajectory + pattern[1, :] + np.random.normal(0, 0.02, np.shape(trajectory))
+    #marker2 = trajectory + pattern[2, :] + np.random.normal(0, 0.02, np.shape(trajectory))
+    #marker3 = trajectory + pattern[3, :] + np.random.normal(0, 0.02, np.shape(trajectory))
+
+    #detections = np.concatenate([marker0, marker1, marker2, marker3], axis=2)
+    #print(np.shape(detections))
     return detections
 
 
 
 [train, test, maxi1, maxi2, maxi3] = gen_training_data()
+[train_quats, test_quats] = gen_quats()
+
 #TODO, check what would be the right scale of the pattern
-pattern = np.array([[0,1,0], [0,0,1], [-1,-1,0], [1, -1, 1]])
-train_dets = add_markers_to_trajectories(train, pattern)
-test_dets = add_markers_to_trajectories(test, pattern)
+pattern = 0.1 * np.array([[0,1,0], [0,0,1], [-1,-1,0], [1, -1, 1]])
+train_dets = add_markers_to_trajectories(train, train_quats, pattern)
+test_dets = add_markers_to_trajectories(test, test_quats, pattern)
 
 #print(np.squeeze(X_train[:,2,:]))
 X_train = torch.from_numpy(train_dets).float()
@@ -97,21 +143,31 @@ class LSTMTracker(nn.Module):
 
     def __init__(self, embedding_dim, hidden_dim):
         super(LSTMTracker, self).__init__()
+
         self.embedding_dim = embedding_dim
         self.hidden_dim = hidden_dim
 
-        self.marker_embedding = nn.Linear(12, embedding_dim)
+        self.fc1 = nn.Linear(input_dim, fc1_dim)
+
+        self.marker_embedding = nn.Linear(fc1_dim, embedding_dim)
+
         # The LSTM takes word embeddings as inputs, and outputs hidden states
         # with dimensionality hidden_dim.
         self.lstm = nn.LSTM(embedding_dim, hidden_dim)
 
+        self.fc2 = nn.Linear(hidden_dim, fc2_dim)
+
         # The linear layer that maps from hidden state space to tag space
-        self.hidden2pos = nn.Linear(hidden_dim, 3)
+
+        self.hidden2pos = nn.Linear(fc2_dim, output_dim)
+
 
     def forward(self, marker_detections):
-        embeddings = self.marker_embedding(marker_detections)
+        x = F.relu(self.fc1(marker_detections))
+        embeddings = F.relu(self.marker_embedding(x))
         lstm_out, _ = self.lstm(embeddings.view(len(marker_detections), -1, self.embedding_dim))
-        pos_space = self.hidden2pos(lstm_out)
+        x = F.relu(self.fc2(lstm_out))
+        pos_space = self.hidden2pos(x)
         #next_pos = F.tanh(pos_space)
         return pos_space
 
@@ -120,6 +176,8 @@ model = LSTMTracker(embedding_dim, hidden_dim)
 model = model.float()
 loss_function = nn.MSELoss()
 optimizer = optim.Adam(model.parameters(), lr=0.01)
+
+#summary(model, input_size=())
 
 def train():
     model.train()
@@ -157,7 +215,7 @@ def eval():
             #print(torch.squeeze(tag_scores[:,n,:]))
             data = torch.stack((tag_scores[:,n,:], Y_test[1:,n,:]),1)
             center = torch.mean(data, dim=(0,1))
-            data = data - centerm
+            data = data - center
             print(data.shape)
             visualize(data)
 
