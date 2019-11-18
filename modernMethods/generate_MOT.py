56d181bb24ed1704b4b67bb42f0be89c9fdad1f1
SimonGiebenhain <simon.giebenhain@uni-konstanz.de>
2019-11-18 17:03:41 +0100

    finished data generation for all settings

--- modernMethods/generate_MOT.py
+++ modernMethods/generate_MOT.py
@@ -7,65 +7,6 @@ import scipy.io
 
 from BehaviourModel import NoiseModelFN, NoiseModelFP
 
-FN_transitions = [{'all': [0.95, 0.04, 0.01], 'some': [0.16, 0.83, 0.01], 'none': [0.66, 0.30, 0.04]},
-                  {'all': [0.93, 0.06, 0.01], 'some': [0.14, 0.85, 0.01], 'none': [0.61, 0.35, 0.04]},
-                  {'all': [0.91, 0.08, 0.01], 'some': [0.12, 0.87, 0.01], 'none': [0.56, 0.40, 0.04]},
-                  {'all': [0.89, 0.10, 0.01], 'some': [0.10, 0.89, 0.01], 'none': [0.51, 0.45, 0.04]},
-                  {'all': [0.88, 0.10, 0.02], 'some': [0.08, 0.91, 0.01], 'none': [0.48, 0.48, 0.04]},
-                  {'all': [0.87, 0.11, 0.02], 'some': [0.08, 0.91, 0.01], 'none': [0.48, 0.48, 0.04]},
-                  {'all': [0.87, 0.11, 0.02], 'some': [0.06, 0.92, 0.02], 'none': [0.48, 0.48, 0.04]},
-                  {'all': [0.87, 0.11, 0.02], 'some': [0.06, 0.92, 0.02], 'none': [0.48, 0.48, 0.04]},
-                  {'all': [0.85, 0.13, 0.02], 'some': [0.06, 0.90, 0.04], 'none': [0.48, 0.48, 0.04]},
-                  {'all': [0.85, 0.13, 0.02], 'some': [0.04, 0.92, 0.04], 'none': [0.48, 0.48, 0.04]}]
-
-FN_p1s = [[0.01, 0.05], [0.05, 0.10], [0.10, 0.15], [0.1, 0.2], [0.10, 0.20], [0.10, 0.20], [0.10, 0.20], [0.10, 0.20], [0.15, 0.25], [0.15, 0.25]]
-FN_p2s = [[0.05, 0.25], [0.10, 0.25], [0.15, 0.35], [0.2, 0.4], [0.25, 0.50], [0.30, 0.55], [0.35, 0.60], [0.35, 0.60], [0.40, 0.60], [0.45, 0.65]]
-FN_p3s = [[0.25, 0.45], [0.35, 0.45], [0.45, 0.60], [0.4, 0.6], [0.50, 0.70], [0.55, 0.75], [0.60, 0.80], [0.60, 0.85], [0.65, 0.80], [0.65, 0.85]]
-FN_p4s = [[0.45, 0.75], [0.55, 0.75], [0.60, 0.80], [0.6, 0.8], [0.70, 0.85], [0.75, 0.90], [0.80, 0.90], [0.80, 0.95], [0.80, 0.95], [0.80, 0.95]]
-
-
-FP_transitions = [[[0.9, 0.1, 0.0], [0.7, 0.2, 0.1], [0.7, 0.2, 0.1]],
-                  [[0.8, 0.1, 0.1], [0.7, 0.2, 0.1], [0.7, 0.2, 0.1]],
-                  [[0.7, 0.2, 0.1], [0.6, 0.3, 0.1], [0.6, 0.2, 0.2]],
-                  [[0.7, 0.2, 0.1], [0.6, 0.2, 0.2], [0.5, 0.2, 0.3]],
-                  [[0.7, 0.2, 0.1], [0.5, 0.3, 0.2], [0.4, 0.2, 0.4]],
-                  [[0.6, 0.2, 0.2], [0.4, 0.4, 0.2], [0.4, 0.2, 0.4]],
-                  [[0.6, 0.2, 0.2], [0.4, 0.4, 0.2], [0.3, 0.2, 0.5]],
-                  [[0.6, 0.2, 0.2], [0.3, 0.5, 0.2], [0.3, 0.2, 0.5]],
-                  [[0.5, 0.3, 0.2], [0.3, 0.5, 0.2], [0.3, 0.2, 0.5]],
-                  [[0.4, 0.4, 0.2], [0.3, 0.5, 0.2], [0.3, 0.2, 0.5]]]
-
-FP_probs = [0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8]
-FP_scales = [8.5, 9, 9.5, 10, 10.5, 11, 11.5, 12, 12.5, 13]
-FP_radiuses = [125, 120, 115, 110, 105, 100, 95, 90, 85, 80]
-
-#TODO: implement gaussian noise on markers
-Noise_scales = [10, 10, 10, 10, 10, 10, 10, 10, 10, 10]
-
-noise_scale = 0
-
-#TODO: loop over all settings, create different .mat-file for every setting
-
-noise_model_states = ['all', 'some', 'none']
-noise_model_transition_prob = {'all': [ 0.2, 0.5, 0.3], 'some': [0.1, 0.4, 0.5], 'none': [0.1, 0.3, 0.6]}
-noise_model_initial_state = 'all'
-p1 = [0.1, 0.2]
-p2 = [0.35, 0.6]
-p3 = [0.6, 0.85]
-p4 = [0.8, 0.95]
-noise_FN_model = NoiseModelFN(noise_model_states, noise_model_transition_prob, noise_model_initial_state, p1, p2, p3, p4)
-
-
-noise_model_FP_states = [0, 1, 2]
-noise_model_FP_transition_probs = np.array([[0.5, 0.3, 0.2], [0.1, 0.8, 0.1], [0.4, 0.1, 0.5]])
-noise_model_FP_initial_probs = np.array([1, 0, 0])
-fp_scale = 10
-fp_prob = 0.6
-radius = 100
-
-noise_FP_model = NoiseModelFP(noise_model_FP_states, noise_model_FP_transition_probs, noise_model_FP_initial_probs,
-                              fp_scale, fp_prob, radius)
-
 
 
 def plot_trajectories(pos):
@@ -225,43 +166,98 @@ def simulate_markers(pos, quats, visibility, patterns, FPs, scale):
     #scipy.io.savemat('data/matlab/generated_data.mat', dict(D=detections, pos=pos, quat=quats))
     return detections
 
+
+FN_transitions = [{'all': [0.95, 0.04, 0.01], 'some': [0.16, 0.83, 0.01], 'none': [0.66, 0.30, 0.04]},
+                  {'all': [0.93, 0.06, 0.01], 'some': [0.14, 0.85, 0.01], 'none': [0.61, 0.35, 0.04]},
+                  {'all': [0.91, 0.08, 0.01], 'some': [0.12, 0.87, 0.01], 'none': [0.56, 0.40, 0.04]},
+                  {'all': [0.89, 0.10, 0.01], 'some': [0.10, 0.89, 0.01], 'none': [0.51, 0.45, 0.04]},
+                  {'all': [0.88, 0.10, 0.02], 'some': [0.08, 0.91, 0.01], 'none': [0.48, 0.48, 0.04]},
+                  {'all': [0.87, 0.11, 0.02], 'some': [0.08, 0.91, 0.01], 'none': [0.48, 0.48, 0.04]},
+                  {'all': [0.87, 0.11, 0.02], 'some': [0.06, 0.92, 0.02], 'none': [0.48, 0.48, 0.04]},
+                  {'all': [0.87, 0.11, 0.02], 'some': [0.06, 0.92, 0.02], 'none': [0.48, 0.48, 0.04]},
+                  {'all': [0.85, 0.13, 0.02], 'some': [0.06, 0.90, 0.04], 'none': [0.48, 0.48, 0.04]},
+                  {'all': [0.85, 0.13, 0.02], 'some': [0.04, 0.92, 0.04], 'none': [0.48, 0.48, 0.04]}]
+
+FN_p1s = [[0.01, 0.05], [0.05, 0.10], [0.10, 0.15], [0.1, 0.2], [0.10, 0.20], [0.10, 0.20], [0.10, 0.20], [0.10, 0.20], [0.15, 0.25], [0.15, 0.25]]
+FN_p2s = [[0.05, 0.25], [0.10, 0.25], [0.15, 0.35], [0.2, 0.4], [0.25, 0.50], [0.30, 0.55], [0.35, 0.60], [0.35, 0.60], [0.40, 0.60], [0.45, 0.65]]
+FN_p3s = [[0.25, 0.45], [0.35, 0.45], [0.45, 0.60], [0.4, 0.6], [0.50, 0.70], [0.55, 0.75], [0.60, 0.80], [0.60, 0.85], [0.65, 0.80], [0.65, 0.85]]
+FN_p4s = [[0.45, 0.75], [0.55, 0.75], [0.60, 0.80], [0.6, 0.8], [0.70, 0.85], [0.75, 0.90], [0.80, 0.90], [0.80, 0.95], [0.80, 0.95], [0.80, 0.95]]
+
+
+FP_transitions = [[[0.9, 0.1, 0.0], [0.7, 0.2, 0.1], [0.7, 0.2, 0.1]],
+                  [[0.8, 0.1, 0.1], [0.7, 0.2, 0.1], [0.7, 0.2, 0.1]],
+                  [[0.7, 0.2, 0.1], [0.6, 0.3, 0.1], [0.6, 0.2, 0.2]],
+                  [[0.7, 0.2, 0.1], [0.6, 0.2, 0.2], [0.5, 0.2, 0.3]],
+                  [[0.7, 0.2, 0.1], [0.5, 0.3, 0.2], [0.4, 0.2, 0.4]],
+                  [[0.6, 0.2, 0.2], [0.4, 0.4, 0.2], [0.4, 0.2, 0.4]],
+                  [[0.6, 0.2, 0.2], [0.4, 0.4, 0.2], [0.3, 0.2, 0.5]],
+                  [[0.6, 0.2, 0.2], [0.3, 0.5, 0.2], [0.3, 0.2, 0.5]],
+                  [[0.5, 0.3, 0.2], [0.3, 0.5, 0.2], [0.3, 0.2, 0.5]],
+                  [[0.4, 0.4, 0.2], [0.3, 0.5, 0.2], [0.3, 0.2, 0.5]]]
+
+FP_probs = [0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8]
+FP_scales = [8.5, 9, 9.5, 10, 10.5, 11, 11.5, 12, 12.5, 13]
+FP_radiuses = [125, 120, 115, 110, 105, 100, 95, 90, 85, 80]
+
+noise_scales = [0, 0, 0.5, 0.5, 1, 1, 1.5, 1.5, 2, 2]
+
 T = 20000
-marker_visibility = np.zeros([10, T, 4])
-for i in range(10):
-    marker_visibility[i, :, :] = noise_FN_model.rollout(T)
-
-false_positives = []
-for i in range(10):
-    noise_FP_model = NoiseModelFP(noise_model_FP_states, noise_model_FP_transition_probs, noise_model_FP_initial_probs,
-                                  fp_scale, fp_prob, radius)
-    false_positives.append(noise_FP_model.rollout(T))
-
-posIntervals, intervals = generate_pos()
-for pos in posIntervals:
-    print(pos.shape)
-quatIntervals = generate_quat(intervals)
-for quat in quatIntervals:
-    print(quat.shape)
-
 patterns = np.load('data/patterns.npy')
 
-marker_vis_intervals = []
-fp_intervals =[]
-for interval in intervals:
-    marker_vis_intervals.append(marker_visibility[:, interval[0]:interval[1]+1, :])
-    fp_interval = []
+for i in range(len(noise_scales)):
+    noise_scale = noise_scales[i]
+
+    noise_model_states = ['all', 'some', 'none']
+    noise_model_transition_prob = FN_transitions[i]
+    noise_model_initial_state = 'all'
+    p1 = FN_p1s[i]
+    p2 = FN_p2s[i]
+    p3 = FN_p3s[i]
+    p4 = FN_p4s[i]
+    noise_FN_model = NoiseModelFN(noise_model_states, noise_model_transition_prob, noise_model_initial_state, p1, p2, p3, p4)
+
+
+    noise_model_FP_states = [0, 1, 2]
+    noise_model_FP_transition_probs = FP_transitions[i]
+    noise_model_FP_initial_probs = np.array([1, 0, 0])
+    fp_scale = FP_scales[i]
+    fp_prob = FP_probs[i]
+    radius = FP_radiuses[i]
+
+    noise_FP_model = NoiseModelFP(noise_model_FP_states, noise_model_FP_transition_probs, noise_model_FP_initial_probs,
+                                     fp_scale, fp_prob, radius)
+
+    marker_visibility = np.zeros([10, T, 4])
     for i in range(10):
-        fp_interval.append(false_positives[i][interval[0]:interval[1]+1])
-    fp_intervals.append(fp_interval)
-detIntervals = []
+        marker_visibility[i, :, :] = noise_FN_model.rollout(T)
 
-for i in range(len(intervals)):
-    detIntervals.append(simulate_markers(posIntervals[i], quatIntervals[i], marker_vis_intervals[i], patterns, fp_intervals[i], noise_scale))
+    false_positives = []
+    for i in range(10):
+        noise_FP_model = NoiseModelFP(noise_model_FP_states, noise_model_FP_transition_probs, noise_model_FP_initial_probs,
+                                      fp_scale, fp_prob, radius)
+        false_positives.append(noise_FP_model.rollout(T))
 
-for dets in detIntervals:
-    print(dets.shape)
-#dets = simulate_markers(pos, quats, marker_visibility, patterns, false_positives, noise_scale)
-#print(dets[0, :, :, 0])
-#print(dets.shape)
+    posIntervals, intervals = generate_pos()
+    quatIntervals = generate_quat(intervals)
 
-#print(dets[0, 1:10, 5 , :])
+    marker_vis_intervals = []
+    fp_intervals =[]
+    for interval in intervals:
+        marker_vis_intervals.append(marker_visibility[:, interval[0]:interval[1]+1, :])
+        fp_interval = []
+        for i in range(10):
+            fp_interval.append(false_positives[i][interval[0]:interval[1]+1])
+        fp_intervals.append(fp_interval)
+    detIntervals = []
+
+    for i in range(len(intervals)):
+        detIntervals.append(simulate_markers(posIntervals[i], quatIntervals[i], marker_vis_intervals[i], patterns, fp_intervals[i], noise_scale))
+
+    data_dict = {}
+
+    for i,dets in enumerate(detIntervals):
+        data_dict['D' + str(i)] = dets
+        data_dict['pos' + str(i)] = posIntervals[i]
+        data_dict['quat' + str(i)] = quatIntervals[i]
+
+    scipy.io.savemat('data/matlab/data_difficulty_' + str(i) + '.mat', data_dict)
