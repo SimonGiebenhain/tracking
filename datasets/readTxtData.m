4312e1c24ae6abac1783fc1b874d31247a8ab74a
SimonGiebenhain <simon.giebenhain@uni-konstanz.de>
2020-06-04 00:23:42 +0200

    read new .txt data format

---
+++ datasets/readTxtData.m
@@ -0,0 +1,67 @@
+function [outputArg1,outputArg2] = readTxtData(file)
+%READTXTDATA Summary of this function goes here
+%   Detailed explanation goes here
+
+nLines = 90000;
+fprintf('Starting to process .txt file.\n')
+fid = fopen(file);
+
+splittedName = strsplit(file, '.');
+storageName = [splittedName{1}, '.mat'];
+fprintf(['The formatted data will be stored at ', storageName, '\n'])
+
+nColumns = 50;
+data = NaN * ones(nLines, nColumns, 3);
+
+%% Load VICON detections
+tline = fgetl(fid);
+percentDone = 0.1;
+firstFrameFound = 0;
+firstFrame = -1;
+t = -1;
+while ischar(tline)
+    if t/nLines > percentDone
+        fprintf([num2str(percentDone*100), 'percent done!\n'])
+        percentDone = percentDone + 0.1;
+    end
+    [startIdx, endIdx] = regexp(tline, 'Frame Number: \d*');
+    if ~isempty(startIdx) && startIdx == 1
+        t = str2num(tline(startIdx+14:endIdx));
+        if firstFrame >= 0 && t+1 == firstFrame
+            break
+        end
+    elseif ~isempty(regexp(tline, '\s*Unlabeled Markers (', 'once'))
+        [startIdx, endIdx] = regexp(tline, '\d*');
+        numDets = str2num(tline(startIdx:endIdx)); 
+        for i=1:numDets
+            tline = fgetl(fid);
+            [startIdx, endIdx] = regexp(tline, '\(.*\)');
+            allDets = tline(startIdx+1:endIdx-1);
+            dets = split(allDets, ',');
+            data(t+1, i, 1) = str2double(dets{1});
+            data(t+1, i, 2) = str2double(dets{2});
+            data(t+1, i, 3) = str2double(dets{3});
+        end
+        if firstFrameFound == 0 && numDets > 0 
+           firstFrame = t+1;
+           firstFrameFound = 1;
+        end
+        
+    end    
+    % line for next iteration
+    tline = fgetl(fid);
+end
+fclose(fid);
+
+
+% shrink data to appropriate size
+empty_cols = all(isnan(squeeze(data(:, :, 1))), 1);
+
+
+formattedData = data(:, ~empty_cols);
+
+save(storageName, 'formattedData');
+
+
+end
+
