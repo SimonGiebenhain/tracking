4355a721d75d896b54b2293030d2790cb72b5c1b
SimonGiebenhain <simon.giebenhain@uni-konstanz.de>
2020-07-02 12:28:50 +0200

    multi threadding detectBackpacks

--- datasets/detectDroppedBackpacks.m
+++ datasets/detectDroppedBackpacks.m
@@ -1,28 +1,34 @@
-%if batchStartupOptionUsed
-%    cd ../..
-%end
-%path = pwd;
-%addpath(genpath([path, '/', 'multiple_object_tracking_project']))
+if batchStartupOptionUsed
+    cd ../..
+end
+path = pwd;
+addpath(genpath([path, '/', 'multiple_object_tracking_project']))
 
 dirName = 'multiple_object_tracking_project/datasets/flock2';
 % This file should contain the names of all .txt files that already have been processed 
 files = getUnprocessedFiles(dirName, -1);
 
- numDets = cell(length(files), 2);
+numDets = zeros(length(files), 1);
+parpool(64)
 
-for i=1:length(files)
+parfor i=1:length(files)
     fname = files{i};
     filePrefix = strsplit(fname, '.');
     filePrefix = filePrefix{1};
     if isfile([filePrefix, '.mat'])
-        load([filePrefix, '.mat']);
+        formattedData = load([filePrefix, '.mat']);
     else
         % old .csv input format: [formattedData, patternsPlusNames] = readVICONcsv(dataFilename, patternDirectoryName);
         formattedData = readTxtData(fname);
     end
     numFrames = sum(any(~isnan(squeeze(formattedData(:, :, 1))), 2));
-    numDets{i,2} = sum(~isnan(formattedData), 'all') / (3*numFrames);
-    numDets{i,1} = fname;
+    numDets(i) = sum(~isnan(formattedData), 'all') / (3*numFrames);
 end
 
-disp(numDets)
+numDetsAndNames = cell{length(files), 2};
+for i=1:length(files)
+   numDetsAndNames{i, 1} = files{i};
+   numDetsAndNames{i, 2} = numDets(i);
+end
+
+save('numberOfDetections.mat', numDetsAndNames)
